be3c5647dbc0438b7145841e347eb8ae14324570
GAndrews <gareth.andrews@digital.hmrc.gov.uk>
2018-01-04 13:58:57 +0000

    Added scala tests for table, feature and flat specs

--- QA/QA_App/test/controllers/HomeControllerSpec.scala
+++ QA/QA_App/test/controllers/HomeControllerSpec.scala
@@ -17,7 +17,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
     "render the index page from a new instance of controller" in {
       val controller = new HomeController(stubControllerComponents())
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+      val home = controller.index(None).apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
