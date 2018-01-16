115e8c57fbd26fda0e332337b5b75f027485072a
GAndrews <gareth.andrews@digital.hmrc.gov.uk>
2018-01-16 15:08:00 +0000

    Updated app

--- QA/QA_Test/src/test/scala/ScalaFeatureTest.scala
+++ QA/QA_Test/src/test/scala/ScalaFeatureTest.scala
@@ -55,4 +55,24 @@ class ScalaFeatureTest extends FeatureSpec with TestSetup with GivenWhenThen wit
       }
   }
 
+  feature("Adding events via the url") {
+    scenario("if I use the URI to add an event"){
+      Given("a user is on the main page")
+      go to host
+
+      val eventName = "Start"
+      val eventDate = "1990-11-08T00:00"
+      val eventType = "Social"
+      val eventDesc = "Start event 2"
+
+      When("they enter the url to add an event")
+      go to "http://localhost:8090/events/add?eventName=Start&eventDate=1990-11-08+00%3A00&eventType=Social&eventDesc=Start+event2"
+      Then("the user is taken to the home page with the event details visible")
+      val table = webDriver.findElement(By.id("eventList")).getText
+        table contains eventName shouldBe true
+        table contains eventDate shouldBe true
+        table contains eventType shouldBe true
+    }
+  }
+
 }
